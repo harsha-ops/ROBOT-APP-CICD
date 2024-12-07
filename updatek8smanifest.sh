61908ca49cccfd89dacfc2e7b8f887493e5611c4
harsha <harsha.venkata220@gmail.com>
2024-12-07 18:28:43 +0000

    Add k8smanifest folder

---
+++ updatek8smanifest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO_URL="https://${GITHUB_TOKEN}@github.com/harsha-ops/ROBOT-APP-CICD.git"
+
+git clone "$REPO_URL" /tmp/repo
+
+cd /tmp/repo
+
+sed -i "s|image:.*|image: $2|g" k8s-manifest/$1-deploy.yml
+
+# Configure Git author details
+git config --global user.email "jenkins@example.com"
+git config --global user.name "Jenkins CI"
+
+git add .
+
+git commit -m "Update kubernetes Manifest"
+
+git push "$REPO_URL"
+
+rm -rf /tmp/repo
